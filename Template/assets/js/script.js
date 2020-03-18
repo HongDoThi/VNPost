58c8a56d0878a31580740c1fc4c6f3d9a99edbf9
HongDoThi <54479317+HongDoThi@users.noreply.github.com>
2020-03-18 10:18:03 +0700

    update

--- Template/assets/js/script.js
+++ Template/assets/js/script.js
@@ -8,7 +8,14 @@ var collapseSub = document.querySelectorAll('collapse-sub');
 var collapseSub_two = document.getElementById('_two');
 var collapseSub_one = document.getElementById('_one');
 var sidebarCollapse_three = document.querySelector('.sidebar-position:nth-child(3) .dflex');
+
+var sidebarPosition = document.querySelector('.sidebar-position:nth-child(3) ');
+
+
+
 var sidebarCollapse_two = document.querySelector('.sidebar-position:nth-child(2) .dflex');
+
+
 var contentCol = document.querySelector('.content__col:nth-child(3)');
 
 
@@ -20,17 +27,12 @@ iconBar.addEventListener('click', () => {
     contentCol.classList.toggle('small');
     collapseSub_one.classList.toggle('pd');
     collapseSub_two.classList.toggle('pd');
-    // collapseSub_one.classList.toggle('show');
+
 
 
 })
 
-// sidebarCollapse_two.addEventListener('click', () => {
 
-//     collapseSub_one.classList.toggle('hidden');
-//     iconPlus.classList.toggle('fa-plus');
-
-// })
 sidebarCollapse_two.addEventListener('click', () => {
     iconMinus__first.classList.toggle('fa-plus');
 
@@ -42,6 +44,7 @@ sidebarCollapse_three.addEventListener('click', () => {
 $(document).ready(function() {
     $('.btn1').click(function() {
         $('#_one').slideToggle();
+        sidebarPosition.classList.toggle('_mt');
     })
 })
 
